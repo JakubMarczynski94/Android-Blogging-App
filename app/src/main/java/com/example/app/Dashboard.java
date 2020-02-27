47249c07de008467ccec4fb6f99c63728b510ab8
philljohnson <philljohnson739@gmail.com>
2020-02-27 23:55:57 +0100

    Solved the issue with current user's name appearing in the list. Now grey and white textboyes appear in the messaging area.

--- app/src/main/java/com/example/app/Dashboard.java
+++ app/src/main/java/com/example/app/Dashboard.java
@@ -64,9 +64,12 @@ public class Dashboard extends AppCompatActivity {
 
     ListView usersList;
     TextView noUsersText;
+
+    // this arraylist grows in size automatically when new elements are added to it, this particular one contains elements that are of the
+    // string data type
+
     ArrayList<String> al = new ArrayList<>();
     int totalUsers = 0;
-    ProgressDialog pd;
 
     // -------------------------------------- end of variables for chat ----------------
 
@@ -303,29 +306,42 @@ public class Dashboard extends AppCompatActivity {
         usersList = (ListView)findViewById(R.id.usersList);
         noUsersText = (TextView)findViewById(R.id.noUsersText);
 
+        // this is the url of our table that contains in the users sub-table the information about the users
         String url = "https://appdata-67dc1.firebaseio.com/users.json";
 
-        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
+        // Finds the text on the internet, and you can retrieve it. With this you make an HTTP request that has to be made
+        // and you parse it as a string. This request specifies the method, the url, and the listeners invoked when you have a success or a failure
+        StringRequest request = new StringRequest(Request.Method.GET, url,
 
-            @Override
-            public void onResponse(String s) {
-                doOnSuccess(s);
-            }
-        },new Response.ErrorListener(){
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                System.out.println("" + volleyError);
-            }
-        });
+                new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String s) {
+                        doOnSuccess(s);
+                    }},
+
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        System.out.println("" + volleyError);
+                    }
+                 });
 
         RequestQueue rQueue = Volley.newRequestQueue(Dashboard.this);
+
+        // meaning you add the above request into the queu, which is managed by the volley class
         rQueue.add(request);
 
+        // adapterview extends viewgroup, generally takes in an array as parameter
+        // OnItemClickListener is an interface definition for a call back to be invoked when an item in this adapterview has been clicked
         usersList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
             @Override
+            // adapterview where the click happened, the view within the adapterview that was clicked, the position of the view within the adapter, the row id of the item that was clicked
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                // chat with the person which was clicked in this array that we are considering, at the right position
                 UserDetails.chatWith = al.get(position);
+                // when clicked we move from this class to the other class, which itself will have a new layout
                 startActivity(new Intent(Dashboard.this, Chat.class));
             }
         });
@@ -357,24 +373,35 @@ public class Dashboard extends AppCompatActivity {
         }
     }
 
-// CHAT PART
+// ---------- CHAT PART
+
+    // when you are indeed able to retrieve the right data from the database of the users, we perform the following action
 
     public void doOnSuccess(String s){
         try {
+
+            // used for client-server communication, stores unordered key-value pairs
             JSONObject obj = new JSONObject(s);
 
+            // in our case the keys must be the uid's
             Iterator i = obj.keys();
             String key = "";
 
             while(i.hasNext()){
-                key = i.next().toString();
 
+                // you transform the Uid to a string
+                key = i.next().toString();
+                String currentUID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+                // then here you should be comparing the key with the current key under consideration, of the current authenticated user
                 if(!key.equals(
-                        UserDetails.email
+                        // UserDetails.email
+                        currentUID
                 )) {
                     al.add(key);
                 }
 
+                // when you discovered new users, you add a user
                 totalUsers++;
             }
 
@@ -382,6 +409,7 @@ public class Dashboard extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        // probably means when you are the only user, then you set the view that there are no users to true
         if(totalUsers <=1){
             noUsersText.setVisibility(View.VISIBLE);
             usersList.setVisibility(View.GONE);
@@ -389,6 +417,12 @@ public class Dashboard extends AppCompatActivity {
         else{
             noUsersText.setVisibility(View.GONE);
             usersList.setVisibility(View.VISIBLE);
+
+            // then you have the usersList listview from the beginning and then you set the corresponding adapter
+            // used to treat a database, file and transform it into UI material
+            // context is the reference of the current class
+            // used to set ou the layout for the list items in which you have a text view
+            // array of objects used to set the textView
             usersList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al));
         }
 
