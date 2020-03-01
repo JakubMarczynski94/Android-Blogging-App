e884fa2b93fa9531d0bae5516191651ca02e1615
webmaster1022 <seniordev1019@gmail.com>
2020-03-01 15:35:34 +0100

    Modifed the dashboard_view layout to include the setup picture and the setup of the name

--- app/src/main/java/com/example/app/Dashboard.java
+++ app/src/main/java/com/example/app/Dashboard.java
@@ -13,6 +13,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -43,6 +44,47 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import id.zelory.compressor.Compressor;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -72,6 +114,26 @@ public class Dashboard extends AppCompatActivity {
     int totalUsers = 0;
 
     // -------------------------------------- end of variables for chat ----------------
+    // ------------------------------------- beginning of variables for setup ------
+
+    private CircleImageView setupImage;
+    private Uri mainImageURI = null;
+
+    private String user_id;
+
+    private boolean isChanged = false;
+
+    private EditText setupName;
+    private Button setupBtn;
+    private ProgressBar setupProgress;
+
+    private StorageReference storageReference;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
+
+    private Bitmap compressedImageFile;
+
+    // ---------------------------- end of variables for setup
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -351,6 +413,9 @@ public class Dashboard extends AppCompatActivity {
                     startActivity(new Intent(Dashboard.this, Chat.class));
                 }
             });
+
+            // -- setup part when user is non null meaning the user is authenticated
+
         }
         // -------------------- END OF CHAT PART ------------------------------------------------------
     }
