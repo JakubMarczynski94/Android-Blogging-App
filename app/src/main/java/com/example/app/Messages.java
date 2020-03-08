f458111b44d40400cd3d60115a762dd700ec5532
philljohnson <philljohnson739@gmail.com>
2020-03-08 23:14:32 +0100

    By using the same recyclerView code used in the comments section for the messages activity, now when you click on messages, you see the image of the other contact appear alongside their name. Also had to introduce a new class "RecyclerItemClickListener". The next steps are :
    1) Understand and annotate the code written thus far, more on recycler views, and understand this extra class added.
    2) Make the number of unread messages appear next to the contacts, or alternatively make a little dot appear signifying that there are unread messages. In that case also make the dot appear next to the button messages. Also make the name not the userid appear in the chat window.
    3) In the search bar, you can search not only for tags but should be able to search for people. When you find a person, you can click on their profile, and it will redirect us to a page with their data/dashboard, and buttons in order to send them messages, or become friends. At the moment you have the whole database appearing in the messages activity, but you should limit this to the approved friends.
    4) When this is done add a functionality to change the text and image of the posts.
    5) There should also be an archive, and you should be able to mark certain posts as public and others as private.
    6) Implement a google maps where you will be able to pinpoint where the post is located and presented, in the apply tab.
    7) Add some new functionality to this apply tab.

--- app/src/main/java/com/example/app/Messages.java
+++ app/src/main/java/com/example/app/Messages.java
@@ -7,8 +7,12 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -16,30 +20,41 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Document;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class Messages extends AppCompatActivity {
 
     // -------------------- beginning of variables for chat --------------
 
-    ListView usersList;
     TextView noUsersText;
 
     ArrayList<String> al = new ArrayList<>();
     int totalUsers = 0;
     private FirebaseAuth auth;
+    private FirebaseFirestore firestore;
+    String username_of_key;
 
+    private RecyclerView users_list;
+    private MessagesAdapter messagesAdapter;
+    private ArrayList<String> usersList;
     // -------------------------------------- end of variables for chat ----------------
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
         auth = FirebaseAuth.getInstance();
@@ -47,9 +62,46 @@ public class Messages extends AppCompatActivity {
         //get current user
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        usersList = (ListView) findViewById(R.id.usersList);
+        users_list = (RecyclerView) findViewById(R.id.usersList);
         noUsersText = (TextView) findViewById(R.id.noUsersText);
+        // --------------------------------------------------
 
+        users_list.addOnItemTouchListener(
+                new RecyclerItemClickListener(getBaseContext(), users_list ,new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override public void onItemClick(View view, int position) {
+
+                        // chat with the person which was clicked in this array that we are considering, at the right position
+                        UserDetails.chatWith = al.get(position);
+                        // when clicked we move from this class to the other class, which itself will have a new layout
+                        startActivity(new Intent(Messages.this, Chat.class));
+                        finish();
+                    }
+
+                    @Override public void onLongItemClick(View view, int position) {
+
+                        // chat with the person which was clicked in this array that we are considering, at the right position
+                        UserDetails.chatWith = al.get(position);
+                        // when clicked we move from this class to the other class, which itself will have a new layout
+                        startActivity(new Intent(Messages.this, Chat.class));
+                        finish();
+                    }
+                })
+        );
+        /*
+        users_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            // adapterview where the click happened, the view within the adapterview that was clicked, the position of the view within the adapter, the row id of the item that was clicked
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // chat with the person which was clicked in this array that we are considering, at the right position
+                UserDetails.chatWith = al.get(position);
+                // when clicked we move from this class to the other class, which itself will have a new layout
+                startActivity(new Intent(Messages.this, Chat.class));
+            }
+        });
+        */
+        // -------------------------------------------------
         if (user != null) {
             // this is the url of our table that contains in the users sub-table the information about the users
             String url = "https://appdata-67dc1.firebaseio.com/users.json";
@@ -80,18 +132,7 @@ public class Messages extends AppCompatActivity {
 
             // adapterview extends viewgroup, generally takes in an array as parameter
             // OnItemClickListener is an interface definition for a call back to be invoked when an item in this adapterview has been clicked
-            usersList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-                @Override
-                // adapterview where the click happened, the view within the adapterview that was clicked, the position of the view within the adapter, the row id of the item that was clicked
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                    // chat with the person which was clicked in this array that we are considering, at the right position
-                    UserDetails.chatWith = al.get(position);
-                    // when clicked we move from this class to the other class, which itself will have a new layout
-                    startActivity(new Intent(Messages.this, Chat.class));
-                }
-            });
         }
     }
 
@@ -101,6 +142,9 @@ public class Messages extends AppCompatActivity {
         // when you are indeed able to retrieve the right data from the database of the users, we perform the following action
 
         public void doOnSuccess(String s){
+
+        firestore = FirebaseFirestore.getInstance();
+
             try {
 
                 // used for client-server communication, stores unordered key-value pairs
@@ -118,7 +162,6 @@ public class Messages extends AppCompatActivity {
 
                     // then here you should be comparing the key with the current key under consideration, of the current authenticated user
                     if (!key.equals(
-                            // UserDetails.email
                             currentUID
                     )) {
                         al.add(key);
@@ -135,17 +178,26 @@ public class Messages extends AppCompatActivity {
             // probably means when you are the only user, then you set the view that there are no users to true
             if (totalUsers <= 1) {
                 noUsersText.setVisibility(View.VISIBLE);
-                usersList.setVisibility(View.GONE);
+                users_list.setVisibility(View.GONE);
             } else {
                 noUsersText.setVisibility(View.GONE);
-                usersList.setVisibility(View.VISIBLE);
+                users_list.setVisibility(View.VISIBLE);
 
                 // then you have the usersList listview from the beginning and then you set the corresponding adapter
                 // used to treat a database, file and transform it into UI material
                 // context is the reference of the current class
                 // used to set ou the layout for the list items in which you have a text view
                 // array of objects used to set the textView
-                usersList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al));
+                // usersList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al));
+
+
+                //RecyclerView Firebase List
+                usersList = al;
+                messagesAdapter = new MessagesAdapter(usersList);
+                users_list.setHasFixedSize(true);
+                users_list.setLayoutManager(new LinearLayoutManager(this));
+                users_list.setAdapter(messagesAdapter);
+
             }
 
         }
@@ -156,7 +208,6 @@ public class Messages extends AppCompatActivity {
         super.onBackPressed();
         startActivity(new Intent(Messages.this, Dashboard.class));
         finish();
-
     }
-    }
+}
 
